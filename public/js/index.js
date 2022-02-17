15867538496df6eca52166b8b72964ce80a60e21
yeye <kambinho123@hotmail.com>
2022-02-17 16:14:33 +0100

    Finally friday

--- public/js/index.js
+++ public/js/index.js
@@ -22,26 +22,17 @@ function createTable(data){
     var appElement = document.getElementById("app")
     var aTable = document.createElement("table")
     appElement.appendChild(aTable)
-    
-    aTable.appendChild(createRow(data[0].name, data[0].points, data[0].logo))
-    aTable.appendChild(createRow(data[1].name, data[1].points, data[1].logo))
-    aTable.appendChild(createRow(data[2].name, data[2].points, data[2].logo))
-    aTable.appendChild(createRow(data[3].name, data[3].points, data[3].logo))
-    aTable.appendChild(createRow(data[4].name, data[4].points, data[4].logo))
-    aTable.appendChild(createRow(data[5].name, data[5].points, data[5].logo))
-    aTable.appendChild(createRow(data[6].name, data[6].points, data[6].logo))
-    aTable.appendChild(createRow(data[7].name, data[7].points, data[7].logo))
-
+    for(resultRow of data.tabell ){
+        aTable.appendChild(resultRow)
+    }
 }
 
-function createRow(name, points, url ) {
+function createRow(resultRow ) {
     var aRow = document.createElement("tr")
-    aRow.appendChild(createImageCell(url))
-    aRow.appendChild(createCell(name))
-    aRow.appendChild(createCell(points))
+    aRow.appendChild(createImageCell(resultRow.url))
+    aRow.appendChild(createCell(resultRow.name))
+    aRow.appendChild(createCell(resultRow.points))
     return aRow
-
-
 }
 
 function createCell(content){
@@ -53,7 +44,7 @@ function createCell(content){
     function createImageCell(url){
         var aCell = document.createElement("td")
         var anImage = document.createElement("img")
-        anImage.src=url
+        anImage.src=url;
         anImage.classList.add("tablelogo")
         aCell.appendChild(anImage)
         return aCell;
